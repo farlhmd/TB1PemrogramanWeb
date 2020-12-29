721ca24860dce178b90e7d7ef3769199ac05310a
farlhmd <m.farhanilhamdi@icloud.com>
2020-12-29 20:02:26 +0700

    Update solusi1.php

--- solusi1.php
+++ solusi1.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "VOKASI";
+$dbname = "FASILKOM";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
