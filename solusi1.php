9c6b70af747b25008204158c37d50543ff3fa5c0
farlhmd <m.farhanilhamdi@icloud.com>
2020-12-30 12:10:13 +0700

    Update solusi1.php

--- solusi1.php
+++ solusi1.php
@@ -1,22 +1,20 @@
 <?php
 //code by: farhan ilhamdi
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "FASILKOM";
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $varMIN = $_POST["varMIN"];
 $varMAX = $_POST["varMAX"]; 
-$sql = "DELETE FROM MAHASISWA WHERE NILAI >= $varMIN and NILAI<=$varMAX";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully: ";
-} else {
-    echo "Error deleting record: " . $conn->error;
-}
-
+$conn->query("DELETE FROM MAHASISWA WHERE NILAI >= $varMIN and NILAI<=$varMAX");
+echo "Record deleted successfully: \n ";
+printf("Data yang terhapus sebanyak: %d\n", $conn->affected_rows);
 $conn->close();
 ?>
